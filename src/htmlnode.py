a1585060471a7e743e820b584e0b5778189c57a7
qqchan5 <qqmore5@gmail.com>
2025-07-22 09:08:00 +0200

    Add LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,7 +11,23 @@ class HTMLNode():
     def props_to_html(self):
         if self.props:
             return "".join(map(lambda kv: f' {kv[0]}="{kv[1]}"', self.props.items()))
-        return None
+        return ""
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        if value is None:
+            raise ValueError("value may not be None")
+        super().__init__(tag, value, props=props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("value is None")
+        if not self.tag:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,15 +1,13 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html(self):
         node = HTMLNode()
-        try:
+        with self.assertRaises(NotImplementedError):
             node.to_html()
-        except Exception as e:
-            self.assertEqual(type(e), NotImplementedError)
 
     def test_props_to_htlm(self):
         d = {
@@ -21,11 +19,48 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_props_to_htlm2(self):
         node = HTMLNode()
-        self.assertEqual(None, node.props_to_html())
+        self.assertEqual("", node.props_to_html())
 
     def test_repr(self):
         node = HTMLNode(tag="p", value="This is a paragraph", props={"href": "a-link.com"})
         self.assertEqual("HTMLNode(p, This is a paragraph, None, {'href': 'a-link.com'})", repr(node))
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_node(self):
+        with self.assertRaises(TypeError):
+            node = LeafNode()
+        with self.assertRaises(TypeError):
+            node = LeafNode(None)
+        with self.assertRaises(TypeError):
+            node = LeafNode(value=None)
+        with self.assertRaises(ValueError):
+            node = LeafNode(None, None)
+
+    def test_leaf_to_html(self):
+        node = LeafNode("p", "Hello, world!")
+        node.value = None
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_raw(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "Hello, world!")
+        self.assertEqual(node.to_html(), "<b>Hello, world!</b>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click", {"href": "http://youtube.com"})
+        self.assertEqual(node.to_html(), '<a href="http://youtube.com">Click</a>')
+
+    def test_repr(self):
+        node = LeafNode("p", "This is a paragraph", {"href": "a-link.com"})
+        self.assertEqual("LeafNode(p, This is a paragraph, {'href': 'a-link.com'})", repr(node))
 
 if __name__ == "__main__":
     unittest.mail()
