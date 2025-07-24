c0a4cadf23444dd6134ba6588a0906a73ffcdebb
qqchan5 <qqmore5@gmail.com>
2025-07-24 19:36:13 +0200

    Add text_to_textnodes()

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -304,5 +304,32 @@ class TestHelperFunctions(unittest.TestCase):
             new_nodes,
         )
 
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes
+        )
+
+    def test_text_to_textnodes_plain(self):
+        nodes = text_to_textnodes("This is just some plain text and should not be devided in any way.")
+        self.assertListEqual(
+            [
+                TextNode("This is just some plain text and should not be devided in any way.", TextType.TEXT),
+            ],
+            nodes
+        )
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -154,3 +154,11 @@ def split_nodes_link2(old_nodes):
             new_nodes.append(TextNode(split_string[1], TextType.TEXT))
     return new_nodes
 
+def text_to_textnodes(text):
+    text_nodes = [TextNode(text, TextType.TEXT)]
+    text_nodes = split_nodes_image(text_nodes)
+    text_nodes = split_nodes_link(text_nodes)
+    text_nodes = split_nodes_delimiter(text_nodes, '**', TextType.BOLD)
+    text_nodes = split_nodes_delimiter(text_nodes, '_', TextType.ITALIC)
+    text_nodes = split_nodes_delimiter(text_nodes, '`', TextType.CODE)
+    return text_nodes
