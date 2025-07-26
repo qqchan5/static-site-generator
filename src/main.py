807b6464f9986bc6882136fd2d60306ecd7ff51f
qqchan5 <qqmore5@gmail.com>
2025-07-26 22:13:54 +0200

    Begin work on main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
-from textnode import *
+#from textnode import *
+import os
+import shutil
+
+LOG_FILE = "static.log"
+
+def copy_dir(dir, log=None):
+    dst = dir.replace("static", "public", 1)
+    if log:
+        log.write(f"mkdir {dst}\n")
+    os.mkdir(f"{dst}")
+    for i in os.listdir(dir):
+        i_path = f"{dir}/{i}"
+        if os.path.isfile(i_path):
+            if log:
+                log.write(f"cp {i_path} to {dst}\n")
+            shutil.copy(i_path, f"{dst}")
+        else:
+            copy_dir(i_path, log)
 
 def main():
-    text_node = TextNode("some text", TextType.IMAGE, "/static/image")
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    with open(LOG_FILE, 'w') as f:
+        copy_dir("static", f)
 
-    print(text_node)
-
-main()
+if __name__ == "__main__":
+    main()
