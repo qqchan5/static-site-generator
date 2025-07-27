f057ca101754b79ad8a9fa881aeee2df10795236
qqchan5 <qqmore5@gmail.com>
2025-07-27 22:26:27 +0200

    Image nodes should have empty self.value
    `not self.value` does not does not support this, change to `self.value
    is not None`. Did same change for self.tag, but it should not matter.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,9 +23,9 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
+        if self.value is None:
             raise ValueError("invalid HTML: no value")
-        if not self.tag:
+        if self.tag is None:
             return self.value
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
 
